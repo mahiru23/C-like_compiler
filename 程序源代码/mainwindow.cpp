8316cbfdcc0467c13d769c26ccf0c540532df1f7
code2player <781941470@qq.com>
2022-02-23 18:27:28 +0800

    c-complier

---
+++ 程序源代码/mainwindow.cpp
@@ -0,0 +1,142 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+#include "QDir"
+#include "QFileDialog"
+#include "QTextStream"
+#include "QFile"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("中间代码生成器");
+
+    connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(InputFile()));
+    connect(ui->pushButton_2,SIGNAL(clicked()),this,SLOT(DoGra()));
+
+    connect(&(GA.lr),SIGNAL(OutStr1(QString,QString)),this,SLOT(out_str1(QString,QString)));
+    connect(&GA,SIGNAL(OutStr2(QString)),this,SLOT(out_str2(QString)));
+    connect(&GA,SIGNAL(OutStr3(QString)),this,SLOT(out_str3(QString)));
+
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+/*从文件夹中选择文件，并将其内容导入plain text edit和文件str*/
+void MainWindow::InputFile()
+{
+    QString path=QDir::toNativeSeparators(QFileDialog::getOpenFileName(this,tr("Save path"),QDir::currentPath()));  //文件路径
+        if(!path.isEmpty())
+        {
+            QFile *file=new QFile;   //申请一个文件指针
+            file->setFileName(path);   //设置文件路径
+            bool ok=file->open(QIODevice::ReadOnly);
+                if(ok)
+                {
+                    QTextStream in(file);
+                    ui->plainTextEdit->clear();
+                    ui->plainTextEdit->appendPlainText(in.readAll());
+                    file->close();
+                }
+        }
+}
+
+void MainWindow::DoGra()
+{
+    GA.CreateGrammerAnalyser();
+    ui->textBrowser->clear();
+    ui->textBrowser_2->clear();
+    ui->textBrowser_3->clear();
+    ui->textBrowser_4->clear();
+
+
+    GA.lr.strFile=ui->plainTextEdit->toPlainText();
+    GA.lr.strToken = "";
+    GA.lr.chPoint = 0;
+    GA.lr.IdPoint = 0;
+    GA.lr.ConstPoint = 0;
+    GA.lr.AnalyseSuccess = 0;
+    GA.lr.StrLength = 0;
+    GA.lr.EndFile=GA.lr.strFile.length();
+
+    GA.lr.PreAnalyse();
+    ui->textBrowser->setText(GA.lr.strFile);
+
+    GA.lr.strToken = "";
+    GA.lr.chPoint = 0;
+    GA.lr.IdPoint = 0;
+    GA.lr.ConstPoint = 0;
+    GA.lr.AnalyseSuccess = 0;
+    GA.lr.StrLength = 0;
+    GA.lr.EndFile=GA.lr.strFile.length();
+
+   // ui->textBrowser->clear();
+
+    while (1)
+    {
+        GA.lr.strToken = "";
+        GA.lr.Analyse();
+        if (GA.lr.chPoint >= GA.lr.EndFile||GA.lr.AnalyseSuccess==1)
+            break;
+    }
+
+    GA.ip=0;
+    GA.nextquad=GA.start_pos;
+    GA.inter_var=0;
+    GA.vtb_place=0;
+
+    LinkList QL_pass1;
+    LinkList QL_pass2;
+    NoPass NUM_pass;//用于表达式等的传递序号
+    GA.QL_pass1=&QL_pass1;
+    GA.QL_pass2=&QL_pass2;
+    GA.NUM_pass=&NUM_pass;
+
+
+    GA.P("Program",1);
+    GA.print_all_Quaternion();
+
+}
+
+
+
+
+void MainWindow::out_str1(QString str1,QString str2)
+{
+    QString ss="";
+    ss+="<$";
+    ss+=str1;
+    ss+=" , ";
+    ss+=str2;
+    ss+=">\n";
+    ui->textBrowser_2->insertPlainText(ss);
+}
+
+
+void MainWindow::out_str2(QString str)
+{
+    str+='\n';
+    ui->textBrowser_3->insertPlainText(str);
+}
+
+void MainWindow::out_str3(QString str)
+{
+    //str+='\n';
+    ui->textBrowser_4->insertPlainText(str);
+}
+
+
+
+
